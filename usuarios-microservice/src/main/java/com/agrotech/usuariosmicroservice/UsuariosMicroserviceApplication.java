2575fd03c295305ef5b0d293c9658d7ed2c52df0
kenny.carreno <kenny.carreno@hexasolutions.co>
2023-09-16 08:23:33 -0500

    ADD - Se agrega microservicio Usuario

---
+++ usuarios-microservice/src/main/java/com/agrotech/usuariosmicroservice/UsuariosMicroserviceApplication.java
@@ -0,0 +1,13 @@
+package com.agrotech.usuariosmicroservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UsuariosMicroserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UsuariosMicroserviceApplication.class, args);
+	}
+
+}
---
+++ usuarios-microservice/src/test/java/com/agrotech/usuariosmicroservice/UsuariosMicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.agrotech.usuariosmicroservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UsuariosMicroserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
